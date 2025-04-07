f1dcd6ff1a603c10d33390676eff0ecaf38931ae
Michael Buckley <michaelcbuckley@proton.me>
2025-04-07 10:08:28 -0400

    Added additional languages I use

--- config/languages/default.nix
+++ config/languages/default.nix
@@ -3,7 +3,11 @@
   enableTreesitter = true;
 
   bash.enable = true;
+  css.enable = true;
+  html.enable = true;
+  sql.enable = true;
   rust.enable = true;
   python.enable = true;
+  yaml.enable = true;
   nix = import ./nix.nix;
 }
